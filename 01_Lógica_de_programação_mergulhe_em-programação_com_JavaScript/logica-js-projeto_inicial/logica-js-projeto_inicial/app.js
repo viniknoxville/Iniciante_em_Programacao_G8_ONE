87dea9886adf8a519ba0ec72fa55ee9454732b6e
viniknoxville <ass.marcosribeiro@gmail.com>
2025-02-17 22:34:17 -0300

    atividade - desafio hora da pratica

---
+++ 01_Lógica_de_programação_mergulhe_em-programação_com_JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,36 @@
+alert("Boas Vindas ao jogo do número secreto");
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto o chte não for igual ao numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    
+    // Se o chute for igual ao número secreto 
+    if (chute == numeroSecreto) {
+       break;
+
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+
+        }
+        // tentativa = tentativas +1;
+        tentativas++
+    }
+
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1){
+    // alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// } else {
+    // alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+// }
